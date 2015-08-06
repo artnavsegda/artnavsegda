ab102f1001d50a57e3845cdbe3aa7bfa1c1fb977
artnavsegda <artnavsegda@gmail.com>
2015-08-06 12:25:35 +0300

    bad dayz

--- xhello.c
+++ xhello.c
@@ -1,81 +1,43 @@
+#include <X11/Xlib.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <X11/Xlib.h>
-#include <GL/glx.h>
-#include <GL/gl.h>
-
-static int dblBuf[] =  {GLX_RGBA, GLX_RED_SIZE, 1, GLX_GREEN_SIZE, 1, GLX_BLUE_SIZE, 1, GLX_DEPTH_SIZE, 12, GLX_DOUBLEBUFFER, None};
-
-Display *dpy;
-Window win;
-
-void fatalError(char *message)
-{
-	fprintf(stderr, "glxsimple: %s\n", message);
-	exit(1);
-}
-
-main(int argc, char **argv)
-{
-	XVisualInfo *vi;
-	Colormap cmap;
-	XSetWindowAttributes swa;
-	GLXContext cx;
-	XEvent event;
-	Bool needRedraw = False, recalcModelView = True;
-	int dummy;
-
-	dpy = XOpenDisplay(NULL);
-	if (dpy == NULL)
-		fatalError("could not open display");
-
-	if (!glXQueryExtension(dpy, &dummy, &dummy))
-		fatalError("X server has no OpenGL GLX extension");
-
-	vi = glXChooseVisual(dpy, DefaultScreen(dpy), dblBuf);
-	cx = glXCreateContext(dpy, vi, None, True);
-	if (cx == NULL)
-		fatalError("could not create rendering context");
-
-	cmap = XCreateColormap(dpy, RootWindow(dpy, vi->screen), vi->visual, AllocNone);
-	swa.colormap = cmap;
-	swa.border_pixel = 0;
-	swa.event_mask = ExposureMask | ButtonPressMask | StructureNotifyMask;
-	win = XCreateWindow(dpy, RootWindow(dpy, vi->screen), 0, 0, 300, 300, 0, vi->depth, InputOutput, vi->visual, CWBorderPixel | CWColormap | CWEventMask, &swa);
-	XSetStandardProperties(dpy, win, "glxsimple", "glxsimple", None, argv, argc, NULL);
-	glXMakeCurrent(dpy, win, cx);
-	XMapWindow(dpy, win);
-	Atom WM_DELETE_WINDOW = XInternAtom(dpy, "WM_DELETE_WINDOW", False);
-	XSetWMProtocols(dpy, win, &WM_DELETE_WINDOW, 1);
-
+#include <string.h>
+ 
+main(void) {
+	Display *d;
+	Window w;
+	XEvent e;
+	char *msg = "Hello, World!";
+	int s;
+ 
+	d = XOpenDisplay(NULL);
+	if (d == NULL)
+	{
+		fprintf(stderr, "Cannot open display\n");
+		exit(1);
+	}
+ 
+	s = DefaultScreen(d);
+	w = XCreateSimpleWindow(d, RootWindow(d, s), 10, 10, 100, 100, 1, BlackPixel(d, s), WhitePixel(d, s));
+	XSelectInput(d, w, ExposureMask | KeyPressMask);
+	XMapWindow(d, w);
+	Atom WM_DELETE_WINDOW = XInternAtom(d, "WM_DELETE_WINDOW", False);
+	XSetWMProtocols(d, w, &WM_DELETE_WINDOW, 1);
+ 
 	while (1)
 	{
-		XNextEvent(dpy, &event);
-		switch (event.type)
-		{
-      			case ConfigureNotify:
-				glViewport(0, 0, event.xconfigure.width, event.xconfigure.height);
-			case Expose:
-				glClearColor(0.0,0.0,0.0,0.0);
-				glClear(GL_COLOR_BUFFER_BIT);
-				glColor3f(1.0, 1.0, 1.0);
-				glMatrixMode(GL_PROJECTION);
-				glLoadIdentity();
-				glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-				glBegin(GL_POLYGON);
-				glVertex2f(-0.5, -0.5);
-				glVertex2f(-0.5, 0.5);
-				glVertex2f(0.5, 0.5);
-				glVertex2f(0.5, -0.5);
-				glEnd();
-				glXSwapBuffers(dpy, win);
-				glFlush();          
-				break;
-			case ClientMessage:
-				XCloseDisplay(dpy);
-				exit(0);
-				break;
+		XNextEvent(d, &e);
+		if (e.type == Expose) {
+			XFillRectangle(d, w, DefaultGC(d, s), 20, 20, 10, 10);
+			XDrawString(d, w, DefaultGC(d, s), 10, 50, msg, strlen(msg));
 		}
+		if (e.type == KeyPress)
+			break;
+		if (e.type == ClientMessage)
+			break;
 	}
+ 
+	XCloseDisplay(d);
 	return 0;
 }
+
