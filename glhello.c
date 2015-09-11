516eef0f353746e7f5b3330c292079ef400d0bf6
artnavsegda <artnavsegda@gmail.com>
2015-09-11 12:54:38 +0300

    glhello from windows finally

--- glhello.c
+++ glhello.c
@@ -1,12 +1,8 @@
 #include <windows.h>
 #include <gl\gl.h>
 
+HMENU menu;
 HPALETTE hPalette = NULL;
-GLfloat x1 = 100.0f;
-GLfloat y1 = 150.0f;
-GLsizei rsize = 50;
-GLfloat xstep = 1.0f;
-GLfloat ystep = 1.0f;
 GLfloat windowWidth;
 GLfloat windowHeight;
 static LPCTSTR lpszAppName = "GLRect";
@@ -32,64 +28,13 @@ void ChangeSize(GLsizei w, GLsizei h)
 	glOrtho(0.0f, windowWidth, 0.0f, windowHeight, 1.0f, -1.0f);
 }
 
-void IdleFunction(void)
-{
-	if(x1 > windowWidth-rsize || x1 < 0)
-		xstep = -xstep;
-	if(y1 > windowHeight-rsize || y1 < 0)
-		ystep = -ystep;
-	if(x1 > windowWidth-rsize)
-		x1 = windowWidth-rsize-1;
-	if(y1 > windowHeight-rsize)
-		y1 = windowHeight-rsize-1;
-	x1 += xstep;
-	y1 += ystep;
-}
-
 void RenderScene(void)
 {
 	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0f, 0.0f, 0.0f);
-	glRectf(x1, y1, x1+rsize, y1+rsize);
+	glRectf(100.0f, 100.0f, 200.0f, 200.0f);
 	glFlush();
-}
-
-HPALETTE GetOpenGLPalette(HDC hDC)
-{
-	HPALETTE hRetPal = NULL;
-	PIXELFORMATDESCRIPTOR pfd;
-	LOGPALETTE *pPal;
-	int nPixelFormat;
-	int nColors;
-	int i;
-	BYTE RedRange,GreenRange,BlueRange;
-	nPixelFormat = GetPixelFormat(hDC);
-	DescribePixelFormat(hDC, nPixelFormat, sizeof(PIXELFORMATDESCRIPTOR), &pfd);
-	if(!(pfd.dwFlags & PFD_NEED_PALETTE))
-		return NULL;
-	nColors = 1 << pfd.cColorBits;	
-	pPal = (LOGPALETTE*)malloc(sizeof(LOGPALETTE) +nColors*sizeof(PALETTEENTRY));
-	pPal->palVersion = 0x300;
-	pPal->palNumEntries = nColors; 
-	RedRange = (1 << pfd.cRedBits) -1;
-	GreenRange = (1 << pfd.cGreenBits) - 1;
-	BlueRange = (1 << pfd.cBlueBits) -1;
-	for(i = 0; i < nColors; i++)
-	{
-		pPal->palPalEntry[i].peRed = (i >> pfd.cRedShift) & RedRange;
-		pPal->palPalEntry[i].peRed = (unsigned char)((double) pPal->palPalEntry[i].peRed * 255.0 / RedRange);
-		pPal->palPalEntry[i].peGreen = (i >> pfd.cGreenShift) & GreenRange;
-		pPal->palPalEntry[i].peGreen = (unsigned char)((double)pPal->palPalEntry[i].peGreen * 255.0 / GreenRange);
-		pPal->palPalEntry[i].peBlue = (i >> pfd.cBlueShift) & BlueRange;
-		pPal->palPalEntry[i].peBlue = (unsigned char)((double)pPal->palPalEntry[i].peBlue * 255.0 / BlueRange);
-		pPal->palPalEntry[i].peFlags = (unsigned char) NULL;
-	}
-	hRetPal = CreatePalette(pPal);
-	SelectPalette(hDC,hRetPal,FALSE);
-	RealizePalette(hDC);
-	free(pPal);
-	return hRetPal;
 }
 
 void SetDCPixelFormat(HDC hDC)
@@ -100,34 +45,21 @@ void SetDCPixelFormat(HDC hDC)
 	SetPixelFormat(hDC, nPixelFormat, &pfd);
 }
 
-int APIENTRY WinMain(HINSTANCE hInstance,HINSTANCE 	hPrevInstance,LPSTR lpCmdLine,int nCmdShow)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
 {
-	MSG			msg;
-	WNDCLASS	wc;
-	HWND		hWnd;
-	wc.style			= CS_HREDRAW | CS_VREDRAW;
-	wc.lpfnWndProc		= (WNDPROC) WndProc;
-	wc.cbClsExtra		= 0;
-	wc.cbWndExtra		= 0;
-	wc.hInstance 		= hInstance;
-	wc.hIcon			= NULL;
-	wc.hCursor			= LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground	= NULL;		
-	wc.lpszMenuName		= NULL;
-	wc.lpszClassName	= lpszAppName;
-	if(RegisterClass(&wc) == 0)
-		return FALSE;
-	hWnd = CreateWindow(lpszAppName,lpszAppName,WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS,100, 100,250, 250,NULL,NULL,hInstance,NULL);
-	if(hWnd == NULL)
-		return FALSE;
-	ShowWindow(hWnd,SW_SHOW);
-	UpdateWindow(hWnd);
-	while( GetMessage(&msg, NULL, 0, 0))
+	MSG Msg;
+	HWND hwnd;
+	WNDCLASS wc = {0,WndProc,0,0,hInstance,LoadIcon(hInstance, "Window"),LoadCursor(NULL, IDC_ARROW),(HBRUSH)(COLOR_WINDOW+1),"Menu","MainWindowClass"};
+	RegisterClass(&wc);
+	menu = GetSubMenu(LoadMenu(hInstance,"Menu"),0);
+	hwnd = CreateWindow("MainWindowClass","Window",WS_OVERLAPPEDWINDOW,CW_USEDEFAULT,CW_USEDEFAULT,300,300,NULL,NULL,hInstance,NULL);
+	ShowWindow(hwnd,SW_SHOWDEFAULT);
+	while(GetMessage(&Msg, NULL, 0, 0) > 0)
 	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
 	}
-	return msg.wParam;
+	return 0;
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd,UINT	message,WPARAM	wParam,LPARAM	lParam)
@@ -141,51 +73,20 @@ LRESULT CALLBACK WndProc(HWND hWnd,UINT	message,WPARAM	wParam,LPARAM	lParam)
 			SetDCPixelFormat(hDC);		
 			hRC = wglCreateContext(hDC);
 			wglMakeCurrent(hDC, hRC);
-			hPalette = GetOpenGLPalette(hDC);
-			SetTimer(hWnd,101,1,NULL);
 			break;
 		case WM_DESTROY:
 			KillTimer(hWnd,101);
 			wglMakeCurrent(hDC,NULL);
 			wglDeleteContext(hRC);
-			if(hPalette != NULL)
-				DeleteObject(hPalette);
 			PostQuitMessage(0);
 			break;
 		case WM_SIZE:
 			ChangeSize(LOWORD(lParam), HIWORD(lParam));
 			break;
-		case WM_TIMER:
-		{
-			IdleFunction();
-			InvalidateRect(hWnd,NULL,FALSE);
-		}
-			break;
 		case WM_PAINT:
-		{
 			RenderScene();
 			SwapBuffers(hDC);
 			ValidateRect(hWnd,NULL);
-		}
-			break;
-		case WM_QUERYNEWPALETTE:
-			if(hPalette)
-			{
-				int nRet;
-				SelectPalette(hDC, hPalette, FALSE);
-				nRet = RealizePalette(hDC);
-				InvalidateRect(hWnd,NULL,FALSE);
-				return nRet;
-			}
-			break;
-		case WM_PALETTECHANGED:
-			if((hPalette != NULL) && ((HWND)wParam != hWnd))
-			{
-				SelectPalette(hDC,hPalette,FALSE);
-				RealizePalette(hDC);
-				UpdateColors(hDC);
-				return 0;
-			}
 			break;
         default:
             return (DefWindowProc(hWnd, message, wParam, lParam));
