841c8d6360d46ecf8e64c1f15f9a0b99b7ef8294
Art Navsegda <artnavsegda@gmail.com>
2015-09-12 15:33:02 +0300

    font3

--- sdlhello.c
+++ sdlhello.c
@@ -3,14 +3,24 @@
 #include <GL/glu.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "font.h"
 
 int main(int argc, char *argv[])
 {
+	GLuint i;
 	SDL_Event event;
 	atexit(SDL_Quit);
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) exit(1);
 	if (SDL_SetVideoMode(300, 300, 0, SDL_OPENGL|SDL_RESIZABLE) == 0) exit(1);
 	glViewport(0, 0, 300, 300);
+	glPixelStorei(GL_UNPACK_ALIGNMENT,1);
+	GLuint fontOffset = glGenLists(128);
+	for (i = 32; i < 127; i++)
+	{
+		glNewList(i+fontOffset, GL_COMPILE);
+		glBitmap(8,13,0.0,2.0,10.0,0.0,font[i-32]);
+		glEndList();
+	}
 	while (1)
 	{
 		while (SDL_PollEvent(&event))
@@ -29,7 +39,19 @@ int main(int argc, char *argv[])
 		glClearColor(0.0, 0.0, 0.0, 0.0);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glColor3f(1.0, 1.0, 1.0);
-		glRectf(-0.5,0.5,0.5,-0.5);
+		//glRectf(-0.5,0.5,0.5,-0.5);
+		/*glBegin(GL_LINE_LOOP);
+			glVertex2f(-0.5,-0.5);
+			glVertex2f(-0.5,0.5);
+			glVertex2f(0.5,0.5);
+			glVertex2f(0.5,-0.5);
+		glEnd();*/
+		glRasterPos2i(0,0);
+		glPushAttrib(GL_LIST_BIT);
+		glListBase(fontOffset);
+		glCallLists(5, GL_UNSIGNED_BYTE,(GLubyte *)"hello");
+		glPopAttrib();
+		glFlush();
 		SDL_GL_SwapBuffers();
 	}
 	return 0;
