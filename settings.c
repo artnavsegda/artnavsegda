fcc6430d5e78c37203bb6e20c03c3c87e19533df
Art Navsegda <artnavsegda@gmail.com>
2015-09-19 13:19:57 +0300

    sadjasdklasdljkasd

--- settings.c
+++ settings.c
@@ -1,12 +1,15 @@
 #include <windows.h>
+#include <stdio.h>
 
 main()
 {
 	char filename[] = ".\\config.ini";
 	char data[10];
-	GetPrivateProfileString("main","setting","default",data,sizeof(data),filename);
 	WritePrivateProfileString("main","setting","changed",filename);
-	GetPrivateProfileInt("main","number",10,filename);
+	GetPrivateProfileString("main","setting","default",data,sizeof(data),filename);
+	puts(data);
+	WritePrivateProfileString("main","number","11",filename);
+	printf("%d\n",GetPrivateProfileInt("main","number",10,filename));
 	return 0;
 }
 
