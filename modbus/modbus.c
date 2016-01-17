28b97994f2443da47827905a7baef16fbb27ff49
Art Navsegda <artnavsegda@gmail.com>
2016-01-17 10:26:09 +0300

    modbus upload

---
+++ modbus/modbus.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <modbus.h>
+
+int main(int argc, char *argv[])
+{
+	modbus_t *mb;
+	uint16_t tab_reg[32];
+
+	if (argc == 1)
+	{
+		printf("name ip adress\n");
+		exit(1);
+	}
+
+	mb = modbus_new_tcp("127.0.0.1", 1502);
+	modbus_connect(mb);
+
+	/* Read 5 registers from the address 0 */
+	modbus_read_registers(mb, 0, 5, tab_reg);
+
+	modbus_close(mb);
+	modbus_free(mb);
+}
