f5b9e7c3f70cc397b713686a8003877bedfa2a72
artnavsegda <artnavsegda@gmail.com>
2016-01-17 12:18:07 +0300

    uppppplplplplp

--- winsock.c
+++ winsock.c
@@ -6,6 +6,16 @@
 
 int main()
 {
+	WSADATA wsaData;
+	int iResult;
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+	if (iResult != 0)
+	{
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
 	return 0;
 }
 
