1897937243e010c5bcabcefefd9ef41e05d955e4
Art Navsegda <artnavsegda@gmail.com>
2016-01-18 20:43:59 +0300

    modbus full stack

--- modbus/modbus.c
+++ modbus/modbus.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <modbus.h>
+#include <errno.h>
 
 int main(int argc, char *argv[])
 {
 	modbus_t *mb;
-	uint16_t tab_reg[32];
+	uint16_t tab_reg[256];
+	int rc;
+	int i;
 
 	if (argc == 1)
 	{
@@ -13,11 +16,24 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	mb = modbus_new_tcp("127.0.0.1", 1502);
-	modbus_connect(mb);
+	mb = modbus_new_tcp(argv[1], 502);
+	if (modbus_connect(mb) == -1)
+	{
+		fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+		modbus_free(mb);
+		return -1;
+	}
 
-	/* Read 5 registers from the address 0 */
-	modbus_read_registers(mb, 0, 5, tab_reg);
+	/* Read 5 registers from the address 10 */
+	rc = modbus_read_registers(mb, 10, 40, tab_reg);
+	if (rc == -1) {
+		fprintf(stderr, "%s\n", modbus_strerror(errno));
+		return -1;
+	}
+
+	for (i=0; i < rc; i=i+2) {
+		printf("reg[%d]=%f (0x%X)\n", i, modbus_get_float(&tab_reg[i]), tab_reg[i]);
+	}
 
 	modbus_close(mb);
 	modbus_free(mb);
