b16ead07969805e422c8f34dced1b25ece520cad
Art Navsegda <artnavsegda@gmail.com>
2016-06-16 09:27:11 +0300

    wtfds

---
+++ modbus/i2cserver.c
@@ -0,0 +1,83 @@
+/*
+ * Copyright © 2008-2010 Stéphane Raimbault <stephane.raimbault@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include <modbus.h>
+
+int main(void)
+{
+    int socket, fd;
+    modbus_t *ctx;
+    modbus_mapping_t *mb_mapping;
+
+    fd = open("/dev/i2c-1",O_RDWR);
+
+
+	if (fd<=1)
+	{
+		printf("error on device\n");
+		exit(1);
+	}
+	int addr = 0x09;
+	if (ioctl(fd,I2C_SLAVE,addr) < 0)
+	{
+		printf("ioctl error\n");
+		exit(1);
+	}
+
+    ctx = modbus_new_tcp("127.0.0.1", 1502);
+    /* modbus_set_debug(ctx, TRUE); */
+
+    mb_mapping = modbus_mapping_new(500, 500, 500, 500);
+    if (mb_mapping == NULL) {
+        fprintf(stderr, "Failed to allocate the mapping: %s\n",
+                modbus_strerror(errno));
+        modbus_free(ctx);
+        return -1;
+    }
+
+    socket = modbus_tcp_listen(ctx, 1);
+    modbus_tcp_accept(ctx, &socket);
+
+    for (;;) {
+        uint8_t query[MODBUS_TCP_MAX_ADU_LENGTH];
+        int rc;
+	mb_mapping->tab_input_registers[0] = 0x16;
+	mb_mapping->tab_registers[100] = i2c_smbus_read_word_data(fd,0x0);
+
+        rc = modbus_receive(ctx, query);
+        if (rc != -1) {
+            /* rc is the query size */
+            modbus_reply(ctx, query, rc, mb_mapping);
+        } else {
+            /* Connection closed by the client or error */
+            break;
+        }
+    }
+
+    printf("Quit the loop: %s\n", modbus_strerror(errno));
+
+    modbus_mapping_free(mb_mapping);
+    modbus_close(ctx);
+    modbus_free(ctx);
+
+    return 0;
+}
