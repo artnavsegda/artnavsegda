c77c2640041f311096836f2a38dfa80fa56dd663
Art Navsegda <artnavsegda@gmail.com>
2016-06-24 17:39:18 +0300

    what

--- modbus/modbus.c
+++ modbus/modbus.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 	for (i=0; i < rc; i++) {
 		printf("reg[%d]=%d (0x%X)\n", i, tab_reg[i], tab_reg[i]);
 	}
-	
+
 	rc = modbus_read_bits(mb, 100, 24, bit_reg);
 	if (rc == -1) {
 		fprintf(stderr, "read registers: %s\n", modbus_strerror(errno));
@@ -45,6 +45,10 @@ int main(int argc, char *argv[])
 	for (i=0; i < rc; i++) {
 		printf("bit[%d]=%d (0x%X)\n", i, bit_reg[i], bit_reg[i]);
 	}
+
+	printf("reg1 - %X\n", modbus_get_byte_from_bits(bit_reg,0,8));
+	printf("reg2 - %X\n", modbus_get_byte_from_bits(bit_reg,8,8));
+	printf("reg3 - %X\n", modbus_get_byte_from_bits(bit_reg,16,8));
 
 	modbus_close(mb);
 	modbus_free(mb);
